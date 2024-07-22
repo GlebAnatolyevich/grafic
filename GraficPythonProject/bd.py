c9df58aa694620c787925ec8e15b3c0464bcc71a
GlebAnatolyevich <66731528+GlebAnatolyevich@users.noreply.github.com>
2024-07-22 18:34:47 +0300

    Add files via upload
    фв

--- GraficPythonProject/bd.py
+++ GraficPythonProject/bd.py
@@ -9,7 +9,7 @@ class BDGraf:
 ### Select ###
 #   Cписок всех таблиц в БД
     def sqlite_master(self):
-        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        self.cursor.execute("SELECT * FROM sqlite_master WHERE type='table';")
         # Получение результатов запроса
         tables = self.cursor.fetchall()
 
@@ -18,31 +18,62 @@ class BDGraf:
             print(table[0])
 
 #   Cписок всех активных работников
-    def select_user(self):
+    def select_users(self):
         self.cursor.execute("SELECT * FROM users ;")
         # Получение результатов запроса
         tables = self.cursor.fetchall()
-
         print(tables)
 
+#   Поиск пользователя по id в таблице users
+    def select_user(self, user_id):
+        self.cursor.execute("SELECT * FROM users WHERE user_id = '" + user_id + "';")
+        tables = self.cursor.fetchall()
+        return tables
+
 #   Поиск смен за день по дате
-    def select_change_date(self,date):
+    def select_chart_date(self,date):
         self.cursor.execute("SELECT * FROM chart WHERE change_date = '"+date+"';")
         tables = self.cursor.fetchall()
         return tables
 
-#   Поиск пользователя по id в таблице users
-    def select_users(self,user_id):
-        self.cursor.execute("SELECT name FROM users WHERE user_id = '" + user_id + "';")
+    def select_chart_no_type_date(self,):
+        self.cursor.execute("SELECT * FROM chart WHERE type_day IS NULL")
         tables = self.cursor.fetchall()
         return tables
 
+
+   # def select_change_date(self, user_id):
+   #     self.cursor.execute("SELECT * FROM chart WHERE user_id = '" + user_id + "';")
+    #    tables = self.cursor.fetchall()
+    #    return tables
+
+
 #   Поиск смены для получения информации о таблице change
-    def select_change(self, change_id):
-        self.cursor.execute("SELECT time_start,time_end FROM change WHERE change_id = '" + change_id + "';")
+    def select_change_id(self, change_id):
+        self.cursor.execute("SELECT * FROM change WHERE change_id = '" + change_id + "';")
         tables = self.cursor.fetchall()
         return tables
 ### Select ###
+
+
+
+
+### insert ###
+
+    def insert_chart(self,change_date,user_id,change_id):
+        self.cursor.execute('INSERT INTO chart ( change_date, user_id,change_id) VALUES ( ?, ?, ?)', ( change_date,user_id,change_id))
+        self.connection.commit()
+
+### insert ###
+
+
+
+
+### UODATE ###
+    def update_chart_type_date(self,chart_id,type_date):
+        self.cursor.execute('UPDATE chart SET type_day = ? WHERE id = ?',(type_date,chart_id))
+        self.connection.commit()
+### UODATE ###
 
 
 #   commit_and_close
--- GraficPythonProject/main.py
+++ GraficPythonProject/main.py
@@ -1,5 +1,7 @@
 import datetime
 from GraficPythonProject.bd import BDGraf
+import json
+import requests
 bd=BDGraf()
 current_date = datetime.datetime.now().strftime('%d.%m.%Y')
 date=""
@@ -21,19 +23,68 @@ date=""
 def working_today(date):
     if(date==""):
         date = current_date
-        return  decoding_record(bd.select_change_date(date))
+        return  decoding_record(bd.select_chart_date(date))
     else:
-        return decoding_record(bd.select_change_date(date))
+        return decoding_record(bd.select_chart_date(date))
 
 def decoding_record(table):
-
+    message = ""
     for line_change_date in table:
-       print(bd.select_users(str(line_change_date[2]))+bd.select_change(str(line_change_date[3])))
+
+        name_user = bd.select_user(str(line_change_date[2]))
+        change_date = bd.select_change_id(str(line_change_date[3]))
+        #print(line_change_date)
+        for user in name_user:
+            message += user[2]
+        for date in change_date:
+            message += ": "+date[2]+ " - "+date[3] + "\n"
+
+    return message
 
 
-def cost_of_shift():
+
+
+#bd.insert_chart(current_date,2,	5)
+
+def check_weekends(date):
+    date_usa=date[-4:]+"-"+date[3]+date[4]+"-"+date[:2]
+    url = 'https://raw.githubusercontent.com/d10xa/holidays-calendar/master/json/consultant' + date[-4:] + '.json'
+    r = requests.get(url)
+    cal = json.loads(r.text)
+    return cal["holidays"].count(date_usa)
+#def shift_analysis(date):
+
+#def select_type_day():
+
+# Ищет где не стоит статус дня и проставляет
+def update_type_day():
+    type_date = ""
+    for date in bd.select_chart_no_type_date():
+       #print(date[1])
+        #print(check_weekends(date[1]))
+
+
+       if(check_weekends(date[1])==1):
+           print("1")
+           bd.update_chart_type_date(date[0],"1")
+
+       else:
+           print("Жопа")
+           bd.update_chart_type_date(date[0], "0")
 
 
 
-#a = "01.06.2024"
+
+#print(bd.select_chart_no_type_date())
+
+    #bd.update_chart_type_date("22","0")
+
+update_type_day()
+#bd.connection_commit_and_close()
+#def insert_chart_day(change_date,user_id,change_id):
+
+#print(check_weekends("08.07.2024"))
+#update_type_day("08.07.2024")
+
+#a = "01.07.2024"
 #print(working_today(a))
